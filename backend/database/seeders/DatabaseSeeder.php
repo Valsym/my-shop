c34bb562c61c47cdef88188c9c1cb3f6ad2343e7
Valsym <fanski@yandex.ru>
2026-03-02 10:02:45 +0300

    Fixed seeder & manifest issues

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -24,7 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProductSeeder::class,
-//            FeedSeeder::class,
+            MainPageSeeder::class,
         ]);
     }
 }
