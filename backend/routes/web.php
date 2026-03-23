d20a27fc1593a5066e3948df773dc86dc4e04367
Valsym <fanski@yandex.ru>
2026-03-23 14:07:11 +0300

    feat: Добавлена пагинация и фильтрация по цене в поиске OpenSearch

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/test-opensearch', function (Client $client) {
 
 use Illuminate\Http\Request;
 // http://localhost/search?q=принтер
-Route::get('/search', function (Request $request, Client $client) {
+Route::get('/search-full', function (Request $request, Client $client) {
    //$query = request('q');//, 'принтер');
     //$query = $request->all();
     $query = $request->query('q');// или request('q')
@@ -47,4 +47,45 @@ Route::get('/search', function (Request $request, Client $client) {
     ];
     $response = $client->search($params);
     return response()->json($response['hits']['hits']);
+});
+
+
+// Пагинация: ?q=принтер&from=0&size=2
+//Фильтр: ?q=принтер&price_min=10000&price_max=50000
+Route::get('/search', function (Request $request, Client $client) {
+    $query = $request->query('q');
+    $from = $request->query('from', 0);
+    $size = $request->query('size', 10);
+    $priceMin = $request->query('price_min');
+    $priceMax = $request->query('price_max');
+
+    $body = [
+        'from' => (int) $from,
+        'size' => (int) $size,
+        'query' => [
+            'bool' => [
+                'must' => [
+                    ['multi_match' => [
+                        'query'  => $query,
+                        'fields' => ['name^3', 'description']
+                    ]]
+                ]
+            ]
+        ]
+    ];
+
+    if ($priceMin !== null || $priceMax !== null) {
+        $range = [];
+        if ($priceMin !== null) $range['gte'] = (float) $priceMin;
+        if ($priceMax !== null) $range['lte'] = (float) $priceMax;
+        $body['query']['bool']['filter'][] = ['range' => ['price' => $range]];
+    }
+
+    $params = ['index' => 'products', 'body' => $body];
+    $response = $client->search($params);
+
+    return response()->json([
+        'total' => $response['hits']['total']['value'],
+        'hits' => collect($response['hits']['hits'])->pluck('_source'),
+    ]);
 });
