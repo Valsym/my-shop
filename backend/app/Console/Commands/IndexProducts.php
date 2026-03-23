5e95d9145a945a3819430ec607352ef0fbccb105
Valsym <fanski@yandex.ru>
2026-03-23 13:44:12 +0300

    feat: OpenSearch интеграция через официальный клиент; удалён pdphilip/opensearch; добавлен полнотекстовый поиск

---
+++ backend/app/Console/Commands/IndexProducts.php
@@ -0,0 +1,47 @@
+<?php
+// sail artisan app:index-products
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+use OpenSearch\Client;
+
+class IndexProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:index-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(Client $client)
+    {
+        $products = Product::all();
+        foreach ($products as $product) {
+            $params = [
+                'index' => 'products',
+                'id'    => $product->id,
+                'body'  => [
+                    'name'        => $product->name,
+                    'description' => $product->description,
+                    'code'        => $product->code,
+                    'price'       => $product->price,
+                ]
+            ];
+            $client->index($params);
+        }
+        $this->info('Индексация завершена');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,3 +19,32 @@ Route::get('/test-opensearch', function (Client $client) {
         return response()->json(['error' => $e->getMessage()], 500);
     }
 });
+
+use Illuminate\Http\Request;
+// http://localhost/search?q=принтер
+Route::get('/search', function (Request $request, Client $client) {
+   //$query = request('q');//, 'принтер');
+    //$query = $request->all();
+    $query = $request->query('q');// или request('q')
+//    $query = $request->get('q');
+    //dd($query);
+    $params = [
+        'index' => 'products',
+        'body'  => [
+            // поиск по фразе:
+//            'query' => [
+//                'match' => [
+//                    'name' => $query
+//                ]
+//            ]
+        // search?q=no* (по по началу фразы)
+            'query' => [
+                'match_phrase_prefix' => [
+                    'name' => $query
+                ]
+            ]
+        ]
+    ];
+    $response = $client->search($params);
+    return response()->json($response['hits']['hits']);
+});
